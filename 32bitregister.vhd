37e583bed7a965fdb19dc79149f39adf2b0f180a
YousefElshabrawy <yousefatefelshabrawy@gmail.com>
2022-01-04 07:33:13 +0200

    The Pc Register

---
+++ 32bitregister.vhd
@@ -0,0 +1,20 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ENTITY register32bit IS
+    PORT (
+        d : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        clk, rst : IN STD_LOGIC;
+        q : OUT STD_LOGIC_VECTOR(31 DOWNTO 0));
+END;
+
+ARCHITECTURE register32bit_a OF register32bit IS
+BEGIN
+    PROCESS (clk, rst)
+    BEGIN
+        IF (Rst = '1') THEN
+            q <= (OTHERS => '0');
+        ELSIF rising_edge (clk) THEN
+            q <= d;
+        END IF;
+    END PROCESS;
+END;
