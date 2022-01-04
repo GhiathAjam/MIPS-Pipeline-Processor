e97851a3026f4562a00ed5699cd6cca74157e151
YousefElshabrawy <yousefatefelshabrawy@gmail.com>
2022-01-04 12:19:08 +0200

    Updating the Fetch Decode Buffer

--- fetch.vhd
+++ fetch.vhd
@@ -7,7 +7,11 @@ ENTITY fetch IS PORT (
   alu_res, mem_res : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
   sel : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
 
-  PCn_O, PC_O, Inst_O : OUT STD_LOGIC_VECTOR(31 DOWNTO 0));
+  PCn_O, PC_O : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+  control_bits : OUT STD_LOGIC_VECTOR(4 DOWNTO 0);
+  rd, rs, rt : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+  offset : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+);
 END;
 
 ARCHITECTURE fetch_a OF fetch IS
@@ -22,6 +26,6 @@ BEGIN
   Mux3x1 : ENTITY work.mux3x1 PORT MAP (adder_res, alu_res, mem_res, sel, hlt, new_pc);
   PC : ENTITY work.register32bit PORT MAP (new_pc, clk, rst, pc_out);
   InstructionMemory : ENTITY work.instructionmemory PORT MAP (pc_out(9 DOWNTO 0), instruction);
-  FetchDecodeBuffer : ENTITY work.fetch_decode_buffer PORT MAP (adder_res, pc_out, instruction, clk, flush, freeze, unfreeze, PCn_O, PC_O, Inst_O);
+  FetchDecodeBuffer : ENTITY work.fetch_decode_buffer PORT MAP (adder_res, pc_out, instruction, clk, flush, freeze, unfreeze, PCn_O, PC_O, control_bits, rd, rs, rt, offset);
 
 END;
--- fetch_decode_buffer.vhd
+++ fetch_decode_buffer.vhd
@@ -7,26 +7,43 @@ ENTITY fetch_decode_buffer IS
   PORT (
     PCn_I, PC_I, Inst_I : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
     clk, flush, freeze, unfreeze : IN STD_LOGIC;
-    
-    PCn_O, PC_O, Inst_O : OUT STD_LOGIC_VECTOR(31 DOWNTO 0));
+
+    PCn_O, PC_O : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+    control_bits : OUT STD_LOGIC_VECTOR(4 DOWNTO 0);
+    rd, rs, rt : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+    offset : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+  );
 END;
 
 ARCHITECTURE fetch_decode_buffer_a OF fetch_decode_buffer IS
 BEGIN
-  PROCESS (flush, freeze, unfreeze, clk)
+  PROCESS (clk, flush)
+    VARIABLE isFrozen : BOOLEAN := false; -- Variable Not Signal to update Immediatly not at the end of process
   BEGIN
     IF flush = '1' THEN
       PCn_O <= (OTHERS => '0');
       PC_O <= (OTHERS => '0');
-      Inst_O <= (OTHERS => '0');
-    ELSIF rising_edge(clk) AND freeze = '0' THEN
-      PCn_O <= (OTHERS => '0');
-      PC_O <= (OTHERS => '0');
-      Inst_O <= (OTHERS => '0');
-    ELSIF rising_edge(clk) AND unfreeze = '1' THEN
-      PCn_O <= PCn_I ;
-      PC_O <= PC_I;
-      Inst_O <= Inst_I;
+      control_bits <= (OTHERS => '0');
+      rd <= (OTHERS => '0');
+      rs <= (OTHERS => '0');
+      rt <= (OTHERS => '0');
+      offset <= (OTHERS => '0');
+    ELSIF falling_edge(clk) THEN
+      IF freeze = '1' THEN
+        isFrozen := true;
+      ELSIF unfreeze = '1' THEN
+        isFrozen := false;
+      END IF;
+      IF NOT isFrozen THEN -- If isFrozzen => latch the old out
+        -- out = in
+        PCn_O <= PCn_I;
+        PC_O <= PC_I;
+        control_bits <= Inst_I(31 DOWNTO 27);
+        rd <= Inst_I(26 DOWNTO 24);
+        rs <= Inst_I(23 DOWNTO 21);
+        rt <= Inst_I(20 DOWNTO 18);
+        offset <= Inst_I(16 DOWNTO 1);
+      END IF;
     END IF;
   END PROCESS;
 END;
